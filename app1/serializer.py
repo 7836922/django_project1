42b5e2cb55bd0b36619796a4b740d9e0dc163dc2
Sneha Prarath <snehaprarath@gmail.com>
2025-11-14 15:01:43 +0530

    first upload

--- app1/serializer.py
+++ app1/serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import trainer
+
 class trainerserializer(serializers.ModelSerializer):
-    class meta:
-        model=trainer
-        fields='__all__'
+    class Meta:
+        model = trainer
+        fields = '__all__'
